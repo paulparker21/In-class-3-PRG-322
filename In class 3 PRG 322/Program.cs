627012ade5a4fe4f2a9d71b20b00ffb8849fa889
paulparker21 <somsh8477@gmail.com>
2020-06-11 20:47:33 -0600

    svae

--- In class 3 PRG 322/Program.cs
+++ In class 3 PRG 322/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace In_class_3_PRG_322
 {
@@ -10,6 +11,21 @@ namespace In_class_3_PRG_322
     {
         static void Main(string[] args)
         {
+            string cs = @"server=54.187.154.185;userid=paulp;password=paulp;database=PAULDB";
+
+            using
+               (var con = new MySqlConnection(cs))
+            {
+                con.Open();
+                Console.WriteLine($"MySQL version: {con.ServerVersion}");
+            }
+                
+
+            
+
+
         }
+
+
     }
 }
---
+++ Inclass3Prg322/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace Inclass3Prg322
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cs = @"server=54.187.154.185;userid=paulp;password=paulp;database=PAULDB";
+
+            using var con = new MySqlConnection(cs);
+            
+                con.Open();
+                Console.WriteLine($"MySQL version: {con.ServerVersion}");
+
+            var stm = "SELECT VERSION()";
+            var cmd = new MySqlCommand(stm, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"MySQL version: {version}");
+
+            insertData(con);
+            
+
+
+
+
+
+        }
+        private static void insertData(MySqlConnection con)
+        {
+            var stm = "Insert into cars (name, price) valuse ('Porche', '19999999')";
+            var cmd = new MySqlCommand(stm, con);
+            cmd.ExecuteNonQuery();
+        }
+
+
+
+    }
+}
