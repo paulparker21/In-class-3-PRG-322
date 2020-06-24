77b6ec4518fcf6a76777fd37561ee292abdafc54
paulparker21 <somsh8477@gmail.com>
2020-06-24 07:14:33 -0600

    yes

--- Inclass3Prg322/Program.cs
+++ Inclass3Prg322/Program.cs
@@ -14,13 +14,13 @@ namespace Inclass3Prg322
                 con.Open();
                 Console.WriteLine($"MySQL version: {con.ServerVersion}");
 
-            var stm = "SELECT VERSION()";
-            var cmd = new MySqlCommand(stm, con);
+            customerReader customerReader = new customerReader(con);
 
-            var version = cmd.ExecuteScalar().ToString();
-            Console.WriteLine($"MySQL version: {version}");
+            customerReader.readAllData();
 
-            insertData(con);
+       
+
+            
             
 
 
@@ -28,13 +28,7 @@ namespace Inclass3Prg322
 
 
         }
-        private static void insertData(MySqlConnection con)
-        {
-            var stm = "Insert into cars (name, price) valuse ('Porche', '19999999')";
-            var cmd = new MySqlCommand(stm, con);
-            cmd.ExecuteNonQuery();
-        }
-
+      
 
 
     }
---
+++ Inclass3Prg322/customerReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Inclass3Prg322
+{
+    public class customerReader
+    {
+        private MySqlConnection con;
+     
+        
+        
+        public customerReader(MySqlConnection con)
+        {
+            this.con = con;
+        
+    
+    
+
+
+    
+
+        }
+
+        public void readAllData()
+        {
+            string sql = "SELECT * FROM customer";
+            using var cmd = new MySqlCommand(sql, con);
+
+            using MySqlDataReader reader = cmd.ExecuteReader();
+            Console.WriteLine("Customer List");
+            while (reader.Read())
+            {
+                Console.WriteLine($"Number: {reader.GetInt32(0)} Direction: {reader.GetString(2)}");
+            }
+        
+        }
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+}
